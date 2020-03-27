7c2da471aea938665b5922be164ceb3ca80d3cf0
guiferrini <guilhermemferrini@gmail.com>
2020-03-27 11:01:01 -0300

    Add page CADASTRO with HTML and CSS

--- aulas/frontend/src/pages/Logon/index.js
+++ aulas/frontend/src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; //Link (to) = a (href), p direcionar não recarregar a pagina, SPA
 import {FiLogIn} from 'react-icons/fi';
 
 import './style.css';
@@ -18,10 +19,10 @@ export default function Logon() {
                     <input placeholder="Sua ID" />
                     <button className="button" type="submit">Entrar</button>
 
-                    <a href="/register">
+                    <Link className='back-link' to="/register">
                         <FiLogIn size={16} color="#E02041" />
                         Não tenho cadastro
-                    </a>
+                    </Link>
                 </form>
             </section>
 
