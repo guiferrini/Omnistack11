243c1abc554d01d0f2d0a13e3b4140de29d3fc4c
guiferrini <guilhermemferrini@gmail.com>
2020-03-27 12:00:01 -0300

    Add Page Profile/Casos Cadastrados: HTML and CSS

--- aulas/frontend/src/routes.js
+++ aulas/frontend/src/routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Logon from './pages/Logon';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
+
 
 export default function Routes() {
     return ( //Switch: garante q uma rote só seja executada por momento
@@ -10,6 +12,8 @@ export default function Routes() {
             <Switch> 
                 <Route path="/" exact component={Logon} />
                 <Route path="/register" component={Register} />
+                
+                <Route path="/profile" component={Profile} />
             </Switch>
         </BrowserRouter>
     );
